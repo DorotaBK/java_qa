fbcc08ffc6505c71a194a46b9c5248353dbf1a9c
DorotaBK <dorota.basarab@gmail.com>
2018-03-07 00:07:29 +0100

    leaving unchanging values in the fields (l.3 m.6)

--- addressbook-web-tests/src/test/java/dbk/qacourse/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/dbk/qacourse/addressbook/tests/GroupModificationTests.java
@@ -10,7 +10,7 @@ public class GroupModificationTests extends TestBase {
         app.getNavigationHelper().goToGroupPage();
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("nowa_BBB", "nowa_BBB", "nowa_BBB"));
+        app.getGroupHelper().fillGroupForm(new GroupData("nowa_7", "nowa_7", "nowa_7"));
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
     }
