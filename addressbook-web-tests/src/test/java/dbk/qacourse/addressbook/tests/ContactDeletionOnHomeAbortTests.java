c75bf94292a5924ce2bfb6d4d6b109e9046e0e4d
DorotaBK <dorota.basarab@gmail.com>
2018-03-11 18:21:39 +0100

    Task 9 L. 3: ContactDeletionOnHomeAbortTests

--- addressbook-web-tests/src/test/java/dbk/qacourse/addressbook/tests/ContactDeletionOnHomeAbortTests.java
+++ addressbook-web-tests/src/test/java/dbk/qacourse/addressbook/tests/ContactDeletionOnHomeAbortTests.java
@@ -1,11 +1,18 @@
 package dbk.qacourse.addressbook.tests;
 
+import dbk.qacourse.addressbook.model.ContactData;
 import org.testng.annotations.Test;
 
 public class ContactDeletionOnHomeAbortTests extends TestBase {
 
     @Test
     public void testContactDeletionOnHomeAbort() {
+
+        //checking pre-conditions and providing them if necessary
+        if (!app.getContactHelper().isThereAContact()) {
+            app.getContactHelper().createContact(new ContactData("Edyta", "Klocek", "klocek",
+                    null, "601601601", "eklocek@wp.pl", "[none]"));
+        }
         app.getContactHelper().selectContactToDelete();
         app.getContactHelper().deleteOnHome();
         app.getContactHelper().isAlertPresent();
