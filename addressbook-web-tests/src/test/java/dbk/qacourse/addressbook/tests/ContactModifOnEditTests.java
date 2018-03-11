52a21c4d866d35e60bf393ead8036e7a7d2588d0
DorotaBK <dorota.basarab@gmail.com>
2018-03-11 18:54:20 +0100

    Task 9 L. 3: ContactModifOnEditTests

--- addressbook-web-tests/src/test/java/dbk/qacourse/addressbook/tests/ContactModifOnEditTests.java
+++ addressbook-web-tests/src/test/java/dbk/qacourse/addressbook/tests/ContactModifOnEditTests.java
@@ -7,6 +7,13 @@ public class ContactModifOnEditTests extends TestBase {
 
     @Test
     public void testContactModifOnEdit(){
+
+        //checking pre-conditions and providing them if necessary
+        if (!app.getContactHelper().isThereAContact()) {
+            app.getContactHelper().createContact(new ContactData("Edyta", "Klocek", "klocek",
+                    null, "601601601", "eklocek@wp.pl", "[none]"));
+        }
+
         app.getContactHelper().selectContactToEdit();
         app.getContactHelper().fillContactForm(new ContactData(" Nina", "Nowakowska",
                 "nina","Nowa Jasna 1/2, 10-100 Opole","600700800", "opole@wp.pl",
