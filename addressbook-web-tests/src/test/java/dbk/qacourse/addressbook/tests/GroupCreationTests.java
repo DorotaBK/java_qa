c4f43bc6d88391a58fbb3cea64db0782922444c4
DorotaBK <dorota.basarab@gmail.com>
2018-03-20 14:59:34 +0100

    Java8: stream(), Comparator, Lambda; Group part 1 (L4. m. 9)

--- addressbook-web-tests/src/test/java/dbk/qacourse/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/dbk/qacourse/addressbook/tests/GroupCreationTests.java
@@ -18,20 +18,17 @@ public class GroupCreationTests extends TestBase {
         GroupData group = new GroupData("grupa_4", "grupa_4","grupa 4");
         app.getGroupHelper().createGroup(group);
 
-        // comparing the size of the List before and after
+        // comparison of size of the Lists (before and after)
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() + 1);
         System.out.println("number of groups at the end: " + after.size());
 
-        // comparing of whole collections (requires conversion of the list into a set)
-        int max = 0;
-        for (GroupData gr : after) {
-            if (gr.getId() > max) {
-                max = gr.getId();
-            }
-        }
-        group.setId(max);
+        // Comparison of whole collections in Java8
+        // step 1: max object found with lambda expression -> get his id -> new group has max id:
+        group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(group);
+
+        // step 2: convert list into set and comparison of old and new collections:
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 }
