07765621ea007f327f35cb0e88a0e4f32e8bc86c
DorotaBK <dorota.basarab@gmail.com>
2018-04-23 00:18:01 +0200

    XML data file, XStream, serialization&deserialization, new Provider (L.6 m.6)

--- addressbook-web-tests/src/test/java/dbk/qacourse/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/dbk/qacourse/addressbook/tests/GroupCreationTests.java
@@ -1,5 +1,6 @@
 package dbk.qacourse.addressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
 import dbk.qacourse.addressbook.model.GroupData;
 import dbk.qacourse.addressbook.model.Groups;
 import org.testng.annotations.DataProvider;
@@ -9,20 +10,36 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
+    // for XML data file:
+    @DataProvider
+    public Iterator<Object[]> validGroups() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
+        String xml = "";
+        String line = reader.readLine();
+        while (line != null){
+            xml += line;
+            line = reader.readLine();
+        }
+        XStream xstream = new XStream();
+        xstream.processAnnotations(GroupData.class);
+        List<GroupData> groups = (List<GroupData>) xstream.fromXML(xml);
+        return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+    }
+
+    /* for CSV data file:
     @DataProvider
     public Iterator<Object[]> validGroups() throws IOException {
         List<Object[]> list = new ArrayList<Object[]>();
-        //list.add(new Object[]{new GroupData().withName("testA").withHeader("header A").withFooter("footer A")});
-        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.")));
         String line = reader.readLine();
         while (line != null){
             String[] split = line.split(";");
@@ -31,6 +48,7 @@ public class GroupCreationTests extends TestBase {
         }
         return list.iterator();
     }
+    */
 
     @Test(dataProvider = "validGroups")
     public void testGroupCreation(GroupData group) {
