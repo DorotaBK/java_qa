e32d9a43f855f731a8c4fba3844a2e9dee99f102
DorotaBK <dorota.basarab@gmail.com>
2018-04-23 22:14:26 +0200

    XML data file, XStream, serialization: Contacts (L.6 m.6)

--- addressbook-web-tests/src/test/java/dbk/qacourse/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/dbk/qacourse/addressbook/generators/ContactDataGenerator.java
@@ -3,6 +3,7 @@ package dbk.qacourse.addressbook.generators;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.thoughtworks.xstream.XStream;
 import dbk.qacourse.addressbook.model.ContactData;
 
 import java.io.File;
@@ -20,6 +21,9 @@ public class ContactDataGenerator {
     @Parameter(names = "-f", description = "Target file")
     public String file;
 
+    @Parameter(names = "-d", description = "Data format")
+    public String format;
+
     public static void main(String[] args) throws IOException {
         ContactDataGenerator generator = new ContactDataGenerator();
         JCommander jCommander = new JCommander(generator);
@@ -34,7 +38,13 @@ public class ContactDataGenerator {
 
     private void run() throws IOException {
         List<ContactData> contacts = generateContacts(count);
-        save(contacts, new File(file));
+        if (format.equals("csv")) {
+            saveAsCSV(contacts, new File(file));
+        } else if (format.equals("xml")) {
+            saveAsXML(contacts, new File(file));
+        } else {
+            System.out.println("Unrecognized format " + format);
+        }
     }
 
     private List<ContactData> generateContacts(int count) {
@@ -43,21 +53,30 @@ public class ContactDataGenerator {
         String[] lastnames = {"Pierwszy", "Drugi", "Trzeci", "Czwarty"};
         String[] emails = {"Pierwszy@wp.pl", "Drugi@wp.pl", "Trzeci@wp.pl", "Czwarty@wp.pl"};
         String[] mobiles = {"500500500", "500600700", "500700800", "500800900"};
-        for(int i=0; i<count; i++){
+        for (int i = 0; i < count; i++) {
             contacts.add(new ContactData().withFirstname(firstnames[i]).withLastname(lastnames[i])
-                        .withEmail(emails[i]).withMobilePhone(mobiles[i]));
+                    .withEmail(emails[i]).withMobilePhone(mobiles[i]));
         }
         return contacts;
     }
 
-    private void save(List<ContactData> contacts, File file) throws IOException {
+    private void saveAsCSV(List<ContactData> contacts, File file) throws IOException {
         System.out.println(new File(".").getAbsolutePath());
         Writer writer = new FileWriter(file);
-        for (ContactData contact : contacts){
+        for (ContactData contact : contacts) {
             writer.write(String.format("%s;%s;%s;%s\n", contact.getFirstname(), contact.getLastname(),
                     contact.getEmail(), contact.getMobilePhone()));
         }
         writer.close(); //close the file
+    }
+
+    private void saveAsXML(List<ContactData> contacts, File file) throws IOException {
+        XStream xstream = new XStream();
+        xstream.processAnnotations(ContactData.class);
+        String xml = xstream.toXML(contacts);
+        Writer writer = new FileWriter(file);
+        writer.write(xml);
+        writer.close();
     }
 
 }
