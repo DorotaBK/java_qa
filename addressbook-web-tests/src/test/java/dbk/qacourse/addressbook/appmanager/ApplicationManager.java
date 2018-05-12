21712cf9dc6a0c60920cdf151051c7215d0daea0
DorotaBK <dorota.basarab@gmail.com>
2018-05-13 00:39:32 +0200

    Hibernatem - download data from the database

--- addressbook-web-tests/src/test/java/dbk/qacourse/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/dbk/qacourse/addressbook/appmanager/ApplicationManager.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
@@ -24,6 +23,7 @@ public class ApplicationManager {
     private NavigationHelper navigationHelper;  // declaration of new NavigationHelper() initialization
     private GroupHelper groupHelper;            // declaration of new GroupHelper() initialization
     private String browser;
+    private DbHelper dbHelper;
 
     public ApplicationManager(String browser) {
         this.browser = browser;
@@ -33,6 +33,8 @@ public class ApplicationManager {
     public void init() throws IOException {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+        dbHelper = new DbHelper();
 
         if (browser.equals(BrowserType.FIREFOX)) {
             wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
@@ -66,4 +68,6 @@ public class ApplicationManager {
     public ContactHelper contacts() {
         return contactHelper;
     }
+
+    public DbHelper db() {return dbHelper;}
 }
