d03f46374886a85a8da98e8b968fa69d23bd2a34
DorotaBK <dorota.basarab@gmail.com>
2018-06-21 00:04:42 +0200

    Registration test, email server (L.8 m.6) part 3

--- mantis-tests/src/test/java/dbk/qacourse/mantis/model/MailMessage.java
+++ mantis-tests/src/test/java/dbk/qacourse/mantis/model/MailMessage.java
@@ -2,14 +2,11 @@ package dbk.qacourse.mantis.model;
 
 public class MailMessage {
 
-    /*
     public String to;
-  public String text;
+    public String text;
 
-
-  public MailMessage(String to, String text) {
-    this.to = to;
-    this.text = text;
-  }
-     */
+    public MailMessage(String to, String text) {
+        this.to = to;       //recipient
+        this.text = text;   //content
+    }
 }
--- mantis-tests/src/test/java/dbk/qacourse/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/dbk/qacourse/mantis/tests/RegistrationTests.java
@@ -1,8 +1,13 @@
 package dbk.qacourse.mantis.tests;
 
+import dbk.qacourse.mantis.model.MailMessage;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.util.List;
 
 public class RegistrationTests extends TestBase {
 
@@ -12,8 +17,9 @@ public class RegistrationTests extends TestBase {
     }
 
     @Test
-    public void testRegistration() {
-        app.registration().start("user1","user1@localhost.localdomain");
+    public void testRegistration() throws IOException, MessagingException {
+        app.registration().start("user1", "user1@localhost.localdomain");
+        List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
     }
 
     @AfterMethod (alwaysRun = true)
