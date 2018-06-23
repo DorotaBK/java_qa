c75da5d93a4df4ec85290981c4f0f2b9c702e227
DorotaBK <dorota.basarab@gmail.com>
2018-06-24 01:07:38 +0200

    Registration test, email server (L.8 m.6) part 4

--- mantis-tests/src/test/java/dbk/qacourse/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/dbk/qacourse/mantis/tests/RegistrationTests.java
@@ -4,10 +4,13 @@ import dbk.qacourse.mantis.model.MailMessage;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import ru.lanwen.verbalregex.VerbalExpression;
 
 import javax.mail.MessagingException;
 import java.io.IOException;
 import java.util.List;
+
+import static org.testng.Assert.assertTrue;
 
 public class RegistrationTests extends TestBase {
 
@@ -18,8 +21,20 @@ public class RegistrationTests extends TestBase {
 
     @Test
     public void testRegistration() throws IOException, MessagingException {
-        app.registration().start("user1", "user1@localhost.localdomain");
+        String user = "user1";
+        String password = "password";
+        String email = "user1@localhost.localdomain";
+        app.registration().start(user, email);
         List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+        String confirmationLink = findConfirmationLink(mailMessages, email);
+        app.registration().finish(confirmationLink, password);
+        assertTrue(app.newSession().login(user, password));
+    }
+
+    private String findConfirmationLink(List<MailMessage> mailMessages, String email) {
+        MailMessage mailMessage = mailMessages.stream().filter((m) -> m.to.equals(email)).findFirst().get();
+        VerbalExpression regex = VerbalExpression.regex().find("http://").nonSpace().oneOrMore().build();
+        return regex.getText(mailMessage.text);
     }
 
     @AfterMethod (alwaysRun = true)
